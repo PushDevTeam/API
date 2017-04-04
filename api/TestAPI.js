f77a839e93a57b5af09f031621cb6a81772924cd
Ante Grgic <ante16@gmail.com>
2017-04-03 22:25:10 -0500

    console.log TestAPI

--- api/TestAPI.js
+++ api/TestAPI.js
@@ -1,5 +1,8 @@
-/*
+
 module.exports = {
     "get": function (req, res, next) {
+        console.log('req', req);
+        console.log('res', res);
+        console.log('next', next);
     }
-}*/
+}
