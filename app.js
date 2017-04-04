9f9c4d4f222df2fd8cc1fce8f15944a715b338f9
Ante Grgic <ante16@gmail.com>
2017-04-04 02:59:43 -0500

    added ./api back

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var mobile = azureMobileApps({});
 mobile.tables.import('./tables');
 
 // Import the files from the api directory to configure the /api endpoint
-//mobile.api.import('./api');
+mobile.api.import('./api');
 
 // Initialize the database before listening for incoming requests
 // The tables.initialize() method does the initialization asynchronously
