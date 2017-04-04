927128ddae51d621dba36c4b7c2bc6d73afdeb2c
Ante Grgic <ante16@gmail.com>
2017-04-04 00:26:37 -0500

    updated TestAPI.js

--- api/TestAPI.js
+++ api/TestAPI.js
@@ -4,7 +4,9 @@ var app = {
         //console.log('req', req);
         //console.log('res', res);
         //console.log('next', next);
-        req.end('test TestAPI response');
+        //req.end('test TestAPI response');
+        var date = { currentTime: Date.now() };
+        res.status(200).type('application/json').send(date);
     }
 }
 
