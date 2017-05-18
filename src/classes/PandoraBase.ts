bfc4da96645eda24cfdb1c81f07bbf133c3e77c4
Ante Grgic <ante16@gmail.com>
2017-05-18 03:16:06 -0500

    change user account again

--- src/classes/PandoraBase.ts
+++ src/classes/PandoraBase.ts
@@ -1,8 +1,8 @@
 const Anesidora = require('anesidora');
-const USERNAME = 'ante@thepushapp.com';
-const PASSWORD = 'ant3@thepushapp';
-//const USERNAME = 'pushdailydotfit@gmail.com';
-//const PASSWORD = 'pushdailydotfit';
+//const USERNAME = 'ante@thepushapp.com';
+//const PASSWORD = 'ant3@thepushapp';
+const USERNAME = 'pushdailydotfit@gmail.com';
+const PASSWORD = 'pushdailydotfit';
 export class PandoraBase {
     pandora: any = new Anesidora(USERNAME, PASSWORD);
     constructor(){
