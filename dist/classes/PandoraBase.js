32af6a1a461188a586406b8582404f66027deda7
Ante Grgic <ante16@gmail.com>
2017-05-17 21:00:28 -0500

    change pandora account

--- dist/classes/PandoraBase.js
+++ dist/classes/PandoraBase.js
@@ -1,8 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Anesidora = require('anesidora');
-const USERNAME = 'ante@thepushapp.com';
-const PASSWORD = 'ant3@thepushapp';
+//const USERNAME = 'ante@thepushapp.com';
+//const PASSWORD = 'ant3@thepushapp';
+const USERNAME = 'pushdailydotfit@gmail.com';
+const PASSWORD = 'pushdailydotfit';
 class PandoraBase {
     constructor() {
         this.pandora = new Anesidora(USERNAME, PASSWORD);
